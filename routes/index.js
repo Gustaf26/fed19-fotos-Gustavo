9f6afe67abbcde92c16ce0c9795818fc89ad616f
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-05-27 00:46:58 +0200

    doing some refactoring

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/auth_controller');
 const userValidationRules = require('../validation_rules/user');
 
 /* GET / */
+
 router.get('/', (req, res) => {
 	res.send({ status: 'you had me at EHLO' });
 });
@@ -14,14 +15,16 @@ router.get('/', (req, res) => {
 router.post('/register', [userValidationRules.createRules], authController.register);
 
 // add ability to login and get JWT access-token and refresh token
+
 router.post('/login', authController.login);
 
 // add ability to refresh a token
+
 router.post('/refresh', authController.refresh);
 
 // add ability to validate JWT's
+
 router.use('/profile', [auth.validateJwtToken], require('./profile'));
 
-//router.use('/users', require('./users'));
 
 module.exports = router;
