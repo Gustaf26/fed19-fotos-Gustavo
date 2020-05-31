37403980819b8a1ea9e8c4a6142267b091513151
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-05-31 14:51:37 +0200

    refactoring user controller -taking away not needed functions

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,44 +6,13 @@ const bcrypt = require('bcrypt');
 const { matchedData, validationResult } = require('express-validator');
 const models = require('../models');
 
-/**
- * Get all resources
- *
- * GET /
- */
-const index = async (req, res) => {
-	const all_users = await models.User.fetchAll();
-
-	res.send({
-		status: 'success',
-		data: {
-			users: all_users
-		}
-	});
-}
 
 /**
- * Get a specific resource
- *
- * GET /:userId
- */
-const getSingleUser = async (req, res) => {
-	const user = await new models.User({ id: req.params.userId })
-		.fetch({ withRelated: ['fotos'] });
-
-	res.send({
-		status: 'success',
-		data: {
-			user,
-		}
-	});
-}
-
-/**
- * Store a new resource
+ * Store a new user
  *
  * POST /
  */
+
 const store = async (req, res) => {
 	// Finds the validation errors in this request and wraps them in an object with handy functions
 	const errors = validationResult(req);
@@ -90,72 +59,9 @@ const store = async (req, res) => {
 	}
 }
 
-/**
- * Update a specific resource
- *
- * PUT /:userId
- */
-const update = async (req, res) => {
-	const userId = req.params.userId;
 
-	const user = await new models.User({ id: userId }).fetch({ require: false });
-	if (!user) {
-		console.log("User to update was not found.");
-		res.status(404).send({
-			status: 'fail',
-			data: 'User Not Found',
-		});
-		return;
-	}
-
-	// Finds the validation errors in this request and wraps them in an object with handy functions
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		console.log("Update user request failed validation:", errors.array());
-		res.status(422).send({
-			status: 'fail',
-			data: errors.array(),
-		});
-		return;
-	}
-
-	const validData = matchedData(req);
-	try {
-		const updatedUser = await user.save(validData);
-
-		res.send({
-			status: 'success',
-			data: {
-				user: updatedUser,
-			},
-		});
-
-	} catch (error) {
-		res.status(500).send({
-			status: 'error',
-			message: 'Exception thrown in database when updating a new user.',
-		});
-		throw error;
-	}
-}
-
-/**
- * Destroy a specific resource
- *
- * DELETE /:userId
- */
-const destroy = (req, res) => {
-	res.status(405).send({
-		status: 'fail',
-		message: 'Method Not Allowed.',
-	});
-}
 
 module.exports = {
-	index,
-	getSingleUser,
-	show,
+
 	store,
-	//update,
-	destroy,
 }
