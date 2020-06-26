1224558a84ec2fd869d7bf0538af440b7ef32ee5
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-06-26 16:59:23 +0200

    deleted profile-validation / Enough with token authentication

--- routes/photos.js
+++ routes/photos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const photoController = require('../controllers/photo_controller');
-const profileValidationRules = require('../validation_rules/profile');
+const photoValidationRules = require('../validation_rules/photos');
 
 /* Get resource's fotos */
 router.get('/', photoController.getPhotos);
@@ -10,10 +10,10 @@ router.get('/', photoController.getPhotos);
 router.get('/:photoId', photoController.getSinglePhoto);
 
 /* Create a foto to this user's collection */
-router.post('/', [profileValidationRules.createphoto], photoController.storePhoto);
+router.post('/', [photoValidationRules.createphoto], photoController.storePhoto);
 
 /*Update foto's attributes*/
-router.put('/:photoId', [profileValidationRules.updatephoto],photoController.updatePhoto)
+router.put('/:photoId', [photoValidationRules.updatephoto],photoController.updatePhoto)
 
 /* Delete resource's single foto */
 router.delete('/:photoId', photoController.deletePhoto);
--- validation_rules/user.js
+++ validation_rules/user.js
@@ -24,13 +24,9 @@ const createRules = [
 	body('last_name').isLength({ min: 2 }),
 ];
 
-// const updateRules = [
-// 	body('password').optional().isLength({ min: 3 }),
-// 	body('first_name').optional().isLength({ min: 2 }),
-// 	body('last_name').optional().isLength({ min: 2 }),
-// ];
 
 module.exports = {
+	
 	createRules,
-	//updateRules,
+	
 }
