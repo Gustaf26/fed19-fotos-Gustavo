08fcd709a4934a7e1f0712c73c9390e50029ea9c
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-06-26 15:38:51 +0200

    different routes for albums and fotos separated -working fine

--- routes/photos.js
+++ routes/photos.js
@@ -4,19 +4,19 @@ const fotoController = require('../controllers/foto_controller');
 const profileValidationRules = require('../validation_rules/profile');
 
 /* Get resource's fotos */
-router.get('/photos', fotoController.getFotos);
+router.get('/', fotoController.getFotos);
 
 /* Get resource's single foto */
-router.get('/photos/:photoId', fotoController.getSingleFoto);
+router.get('/:photoId', fotoController.getSingleFoto);
 
 /* Create a foto to this user's collection */
-router.post('/photos', [profileValidationRules.createfoto], fotoController.storeFoto);
+router.post('/', [profileValidationRules.createfoto], fotoController.storeFoto);
 
 /*Update foto's attributes*/
-router.put('/photos/:photoId', [profileValidationRules.updatefoto],fotoController.updateFoto)
+router.put('/:photoId', [profileValidationRules.updatefoto],fotoController.updateFoto)
 
 /* Delete resource's single foto */
-router.delete('/photos/:photoId', fotoController.deleteFoto);
+router.delete('/:photoId', fotoController.deleteFoto);
 
 
 module.exports = router;
